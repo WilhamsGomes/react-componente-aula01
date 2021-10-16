370c07f95478689927840b3c55aa7396565cbe25
WilhamsGomes <wilhamsgoomes@gmail.com>
2021-10-16 14:56:45 -0300

    Enviando atividade

---
+++ aula01/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Mensagem } from './Mensagem.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Mensagem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ aula01/src/Mensagem.js
@@ -0,0 +1,3 @@
+import React from "react"; 
+
+export const Mensagem = () => <h1>Fé que eu vou entender esse tal de ReactJS</h1>
